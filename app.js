d19a3a8e3174b2d289534eabf1f4c5fb24482e67
unknown <nathaliecortz@hotmail.com>
2019-06-04 09:07:09 -0500

    Welcome page, home page and navs without style

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  setTimeout(function() {
+      $(".presentation").fadeOut(1500);
+  },3000);
+
+  setTimeout(function() {
+      $(".homePage").fadeIn(1500);
+  },4000);
+});
