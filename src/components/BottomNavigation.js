0915e8c0df4ffe31add0a1877e8a7619d4ba1078
unknown <nathaliecortz@hotmail.com>
2019-06-06 11:05:53 -0500

    Empty components for Router

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom"
 import './navigation.css'
 export default class BottomNavigation extends Component {
     render() {
@@ -6,11 +7,11 @@ export default class BottomNavigation extends Component {
             <div>
                <nav className= "navigation">
                 <div className="containerNav brackets">               
-                        <a className= "navIcons"><i className="lni-home "></i></a>
-                        <a className= "navIcons"><i className="lni-user"></i></a>
-                        <a className= "navIcons"><i className="lni-code-alt"></i></a>
-                        <a className= "navIcons"><i className="lni-folder"></i></a>
-                        <a className= "navIcons"><i className="lni-envelope lni-burst-effect"></i></a>                       
+                   <Link to="/"><a className= "navIcons"><i className="lni-home "></i></a></Link>     
+                      <Link to="/about"> <a className= "navIcons"><i className="lni-user"></i></a></Link> 
+                      <Link to="/knowledge">  <a className= "navIcons"><i className="lni-code-alt"></i></a></Link> 
+                      <Link to="/proyects"> <a className= "navIcons"><i className="lni-folder"></i></a></Link> 
+                      <Link to="/contact"> <a className= "navIcons"><i className="lni-envelope lni-burst-effect"></i></a>     </Link>                   
                       </div>  
         </nav> 
             </div>
