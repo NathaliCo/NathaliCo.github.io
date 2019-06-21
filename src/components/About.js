f2b4b21775d58bd237cff2a95fd6d6fc63885633
unknown <nathaliecortz@hotmail.com>
2019-06-21 13:56:55 -0500

    Fixing style in welcome page

--- src/components/About.js
+++ src/components/About.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react'
 import SocialNetworksBar from './SocialNetworksBar'
 import BottomNavigation from './BottomNavigation'
 import Title from './Title';
-
+import './about.css'
 
 export default class About extends Component {
     render() {
         return (
-            <div>
+            <React.Fragment>
               <SocialNetworksBar/>
-          <Title  name = "About me"/>
+              <div className = "about">
+              <Title  name = "About me"/>
 
-                <p>Front-end developer, antropologist y Musician. One of my favorite activities, besides literature, is to learn new things, which is why I decided to enter to the tech world. My long-term goal is to be able to generate social change through my code. I am interested in being updated on new technologies, so I look for a place where I can continue developing my skills and abilities as a web developer. Although my first approach with the programming has been in front end, I would like later to learn about back-end technologies. </p>
+                <p className= "descriptionAbout">Front-end developer, antropologist y Musician. One of my favorite activities, besides literature, is to learn new things, which is why I decided to enter to the tech world. My long-term goal is to be able to generate social change through my code. I am interested in being updated on new technologies, so I look for a place where I can continue developing my skills and abilities as a web developer. Although my first approach with the programming has been in front end, I would like later to learn about back-end technologies. </p>
                
                 <a href="../assets/Martha Nathalie Cortez Chávez (Front-end).pdf" download="Martha Nathalie Cortez Chávez (Front-end)">
                 Download CV
                 </a>
+                </div>
                 <BottomNavigation/>
-            </div>
+            </React.Fragment>
         )
     }
 }
