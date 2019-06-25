5c03b8a23bfd60fa3a85d5c245977d3471a17c4a
unknown <nathaliecortz@hotmail.com>
2019-06-25 12:49:49 -0500

    Updates

---
+++ precache-manifest.9c83e145abb662a555eb71b704e94aae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1a792bbced02839853452edbf5cf18",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4f1e488f1fb6c739b7d2",
+    "url": "/portfolio/static/css/main.e3f6af81.chunk.css"
+  },
+  {
+    "revision": "53f4ab83d0eafa029f25",
+    "url": "/portfolio/static/js/2.c3bef0e5.chunk.js"
+  },
+  {
+    "revision": "4f1e488f1fb6c739b7d2",
+    "url": "/portfolio/static/js/main.19792f7a.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
