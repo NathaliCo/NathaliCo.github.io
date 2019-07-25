bc464b78b01e23a1396c79aeb794eefe5b78cbe6
Nathalie <nathaliecortz@hotmail.com>
2019-07-25 11:10:00 -0500

    Updates

---
+++ precache-manifest.c121e2b7f61406da6606a8b29a468f75.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641f9000c61322a9b77c3e3f01cc9f11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a9fd9bd33f1c848cf2d",
+    "url": "/static/css/main.cdf703e1.chunk.css"
+  },
+  {
+    "revision": "994e8e794bc06505d842",
+    "url": "/static/js/2.335a1e68.chunk.js"
+  },
+  {
+    "revision": "9a9fd9bd33f1c848cf2d",
+    "url": "/static/js/main.e36ca395.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac37e0396b274d433dd0da6c5a4c47ce",
+    "url": "/static/media/Martha Nathalie Cortez Chávez (Front-end).ac37e039.pdf"
+  },
+  {
+    "revision": "ec3fb23228a83a9a2e64ac4224dadc57",
+    "url": "/static/media/burgerQueen.ec3fb232.png"
+  },
+  {
+    "revision": "8fb81df4b0dc4faa2b5cda03b3317435",
+    "url": "/static/media/cipher.8fb81df4.jpg"
+  },
+  {
+    "revision": "2f710eeac4fe5dd2aba846985469cb0d",
+    "url": "/static/media/dataLovers.2f710eea.jpg"
+  },
+  {
+    "revision": "40c2164a691cae7105859ece361f8857",
+    "url": "/static/media/md-Links.40c2164a.png"
+  },
+  {
+    "revision": "18ea121fc2d1c8f4f698fb5d4915875a",
+    "url": "/static/media/organaHome.18ea121f.png"
+  }
+]);
