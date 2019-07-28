a37c4f060ea882108918e31a76987e579a3212e5
Nathalie <nathaliecortz@hotmail.com>
2019-07-28 12:48:56 -0500

    Add Descriptions to proyects

--- src/Components/ProyectsList.js
+++ src/Components/ProyectsList.js
@@ -30,11 +30,11 @@ export default class ProyectsList extends Component {
           <Title  name = "Proyects"/>
 
                     <div className = "row mt-30">
- <Proyect img={organaHome} name={this.state.proyect[1].name} tools={this.state.proyect[1].tools} enviroment={this.state.proyect[1].enviroment} code = {this.state.proyect[1].code} />
- <Proyect img={burgerQueen} name={this.state.proyect[2].name} tools={this.state.proyect[2].tools} enviroment={this.state.proyect[2].enviroment} code = {this.state.proyect[2].code} />              
- <Proyect img={cipher} name={this.state.proyect[3].name} tools={this.state.proyect[3].tools} enviroment={this.state.proyect[3].enviroment} code = {this.state.proyect[3].code} />
- <Proyect img={mdLinks} name={this.state.proyect[4].name} tools={this.state.proyect[4].tools} enviroment={this.state.proyect[4].enviroment} code = {this.state.proyect[4].code} />     
- <Proyect img={dataLovers} name={this.state.proyect[5].name} tools={this.state.proyect[5].tools} enviroment={this.state.proyect[5].enviroment} code = {this.state.proyect[5].code} />
+ {/*<Proyect img={organaHome} name={this.state.proyect[1].name} tools={this.state.proyect[1].tools} enviroment={this.state.proyect[1].enviroment} code = {this.state.proyect[1].code} description = {this.state.proyect[1].description}/>*/}
+ <Proyect img={burgerQueen} name={this.state.proyect[2].name} tools={this.state.proyect[2].tools} enviroment={this.state.proyect[2].enviroment} code = {this.state.proyect[2].code} description = {this.state.proyect[2].description}/>              
+ <Proyect img={cipher} name={this.state.proyect[3].name} tools={this.state.proyect[3].tools} enviroment={this.state.proyect[3].enviroment} code = {this.state.proyect[3].code} description = {this.state.proyect[3].description}/>
+ <Proyect img={mdLinks} name={this.state.proyect[4].name} tools={this.state.proyect[4].tools} enviroment={this.state.proyect[4].code} code = {this.state.proyect[4].code} description = {this.state.proyect[4].description}/>     
+ <Proyect img={dataLovers} name={this.state.proyect[5].name} tools={this.state.proyect[5].tools} enviroment={this.state.proyect[5].enviroment} code = {this.state.proyect[5].code} description = {this.state.proyect[5].description}/>
                     </div>
                 </div>
                 </ScrollAnimation>
