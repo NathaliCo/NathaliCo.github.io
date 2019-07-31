eecee326879f98f604da71ea955be0436d422711
Nathalie <nathaliecortz@hotmail.com>
2019-07-30 21:14:11 -0500

    Updates

---
+++ precache-manifest.36a2e83aebc047609f296e02d3893a26.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01fc7e2c01fb0ef55ead6ed822e7597e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a659eba57a9b3e917558",
+    "url": "/static/css/main.ae176296.chunk.css"
+  },
+  {
+    "revision": "8a8360169ae2d3e34175",
+    "url": "/static/js/2.25f30d3d.chunk.js"
+  },
+  {
+    "revision": "a659eba57a9b3e917558",
+    "url": "/static/js/main.00bf7a15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3208afdf11eae05faa6c5e7724706884",
+    "url": "/static/media/Nathalie Cortez Front End (English).3208afdf.pdf"
+  },
+  {
+    "revision": "235326d77d41fffe849cb13f1fa6d73d",
+    "url": "/static/media/Nathalie Cortez Front End.235326d7.pdf"
+  },
+  {
+    "revision": "797e7a5de2e024b78a16b1adb1b85215",
+    "url": "/static/media/burgerQueen.797e7a5d.png"
+  },
+  {
+    "revision": "4452c23eb98a1de653fe76bb937b1052",
+    "url": "/static/media/cipher.4452c23e.png"
+  },
+  {
+    "revision": "bfc315268ad4e7a06982eddf6aab2ed2",
+    "url": "/static/media/dataLovers.bfc31526.png"
+  },
+  {
+    "revision": "d29909e9e63dbb6fc2fa9a7abe7b464f",
+    "url": "/static/media/mdLinks.d29909e9.png"
+  }
+]);
